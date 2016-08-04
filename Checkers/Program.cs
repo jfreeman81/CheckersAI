9280a8c1f6156a9084c87140d6b31053aa5076f7
Jordan Freeman <crew2081@gmail.com>
2016-08-04 11:59:00 -0500

    Initial file upload

---
+++ Checkers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Player
+    {
+
+    }
+
+    class Piece
+    {
+
+    }
+
+    class Board
+    {
+
+    }
+
+    class Game
+    {
+
+    }
+
+}
